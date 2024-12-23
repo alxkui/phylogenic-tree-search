9c4d1ca87fa7737ae17400d000a7c70d96e6d82a
alxkui <kovacs571999@gmail.com>
2024-12-23 14:13:19 +0000

    Related groups

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { ChangeEvent, useState } from "react";
+import { BiDna } from "react-icons/bi";
 
 export default function Home() {
   const [data, setData] = useState([]);
@@ -8,11 +9,10 @@ export default function Home() {
   async function handleChange(e: ChangeEvent<HTMLInputElement>) {
     if (e.target.value.length > 3) {
       const response = await fetch(
-        `https://www.ebi.ac.uk/ena/portal/api/search?result=taxon&query=description="${e.target.value}*"&fields=all&limit=10&searchCurations=true&format=json&download=false`
+        `https://www.ebi.ac.uk/ena/portal/api/search?result=taxon&query=description="*${e.target.value}*"&fields=all&limit=10&searchCurations=true&format=json&download=false`
       );
       const dataJson = await response.json();
       setData(dataJson);
-      console.log(dataJson);
       setShowSearch(true);
     } else {
       setShowSearch(false);
@@ -20,34 +20,37 @@ export default function Home() {
   }
 
   return (
-    <div className="grid justify-center align-middle content-center h-screen w-screen font-[family-name:var(--font-geist-sans)]">
-      <form className="bg-[#240b36] rounded-md">
+    <div className="flex flex-col h-3/4 justify-center">
+      <form className="bg-[#240b36] rounded-full w-full">
         <input
           onChange={(e) => handleChange(e)}
-          className="pt-8 pb-8 pl-8 pr-8 rounded-md bg-[#240b36] border-r-2 w-[800] rounded-r-none"
+          className="py-4 px-4 rounded-md bg-[#240b36] w-full border"
           type="text"
-          placeholder="Enterobacter spp."
+          placeholder="Start your search here e.g. Enterobacter sp."
         />
-        <button
-          className="p-8 pl-9 hover:bg-[#c31432] rounded-r-md transition-colors"
-          type="button"
-        >
-          Search
-        </button>
       </form>
-
-      {
-        showSearch && <SearchResults data={data} />
-      }
-
+      { showSearch ? <SearchResults data={data} /> : <Hero /> }
     </div>
   );
 }
 
+function Hero() {
+  return (
+    <div className="flex -z-20 flex-row justify-center items-center mt-28">
+        <div className="w-[400]">
+          <h3 className="text-4xl text-left">Easily find taxonomy information, such as lineage and genetic code.</h3>
+        </div>
+        <div>
+          <BiDna size="300" />
+          </div>
+      </div>
+  )
+}
+
 function SearchResults(props: any) {
   return (
-    <div className="bg-[#c31432] p-6 h-[500px] overflow-scroll">
-      <h2 className="text-2xl">Search results</h2>
+    <div className="bg-[#240b36] p-6 h-[600] relative bottom-1 border-[#391253] border border-t-0 rounded-br-md rounded-bl-md overflow-scroll">
+      <h2 className="text-2xl">Search results {`(${props.data.length})`}</h2>
       <ul>
         <li className="my-4">
           {props.data.map((taxa: any) => (
@@ -68,15 +71,15 @@ function SearchResults(props: any) {
 function TaxaElement(props: any) {
   return (
     <Link href={props.taxaLink}>
-      <div className="flex justify-between hover:bg-[#fc2145] p-6">
+      <div className="flex justify-between hover:bg-[#391253] p-6">
         <div>
           <span>{props.taxaName}</span>
         </div>
         <div>
-          <span className="mr-4 border border-white rounded-full p-2">
+          <span className="mr-4 border bg-white rounded-full p-2 text-sm text-[#391253]">
             Rank: {props.taxaRank}
           </span>
-          <span className="border border-white rounded-full p-2">
+          <span className="border bg-white rounded-full p-2 text-sm text-[#391253]">
             Genetic Code: {props.taxaGeneticCode}
           </span>
         </div>
--- app/taxonomy/page.tsx
+++ app/taxonomy/page.tsx
@@ -2,36 +2,92 @@
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { FaArrowLeft } from "react-icons/fa";
+
+function NestedLineage(props: any) {
+    let margin = 60;
+
+    return(
+        <div className="relative my-4">
+            <span className="deep" style={{
+                paddingLeft: `${margin * props.multiplier}px`,
+                display: 'block'
+            }}>
+                {props.lineage}
+            </span>
+            <div style={{
+                position: "absolute",
+                content: "",
+                width: `${margin * props.multiplier - 10}px`,
+                height: '1px',
+                backgroundColor: 'white',
+                top: '50%',
+                transform: 'translateY(-50%)',
+                left: '0',
+            }}></div>
+        </div>
+    );
+}
 
 export default function Taxonomy() {
     const [taxa, setTaxa]: any = useState([]);
+    const [related, setRelated]: any = useState([]);
+    let lineage: any = [];
     const searchParams = useSearchParams()
     const objectFromQuery: any = searchParams.get('object');
 
     function renderLineage(): any {
         if(taxa) {
-            const lineage = taxa.lineage?.split(';');
-            lineage?.pop();
-            return lineage?.map((l: String, i: number) => (i != lineage?.length - 1) ? <span>{l} &#8594; </span> : <span>{l}</span>)
+            const lineageArr = taxa.lineage?.split(';');
+            lineageArr?.pop();
+            lineage = lineageArr;
+            if(!lineage?.length) return <span>There is no lineage for this taxa, must be a super kingdom</span>
+            return lineage?.map((l: String, i: number) => <NestedLineage lineage={l} multiplier={i} />)
         }
     }
 
     useEffect(() => {
-        return setTaxa(JSON.parse(objectFromQuery));
+        setTaxa(JSON.parse(objectFromQuery));
     }, [])
 
     return(
-        <div className="flex flex-col justify-center h-screen w-screen p-8 font-[family-name:var(--font-geist-sans)]">
-            <Link href="/">Back</Link>
-            <h1 className="text-3xl">Scientific Name: {taxa.scientific_name}</h1>
+        <div className="bg-[#240b36] p-6 rounded-md">
+            <Link href="/"><FaArrowLeft className="mb-4" /></Link>
+            <h1 className="text-4xl">{taxa.scientific_name}</h1>
             <div>
-                <h1 className="text-2xl">Lineage:</h1>
-                {
-                    renderLineage()
-                }
+                <span className="text-sm mr-4 pill">Rank: {taxa.rank}</span>
+                <span className="text-sm pill">Genetic Code: {taxa.genetic_code}</span>
             </div>
-            <h1 className="text-2xl">Rank: {taxa.rank}</h1>
-            <h1 className="text-2xl">Genetic Code: {taxa.genetic_code}</h1>
+            <div className="my-4 border border-[#391253] p-4 rounded-md">
+                <h1 className="text-2xl">Lineage:</h1>
+                { taxa && renderLineage() }
+            </div>
+            <div>
+                { lineage?.length > 0 && <Related rank={taxa.rank} lineage={lineage[lineage?.length - 2]} /> }
+            </div>
         </div>
     );
+}
+
+function Related(props: any) {
+    const [related, setRelated] = useState([]);
+
+    async function getRelated(): Promise<any> {
+        const response = await fetch(`https://www.ebi.ac.uk/ena/portal/api/search?result=taxon&query=lineage="*${props.lineage}*" AND rank="${props.rank}"&fields=all&limit=10&searchCurations=true&format=json&download=false`)
+        const data = await response.json();
+        setRelated(data);
+    }
+
+    useEffect(() => {
+        getRelated();
+    }, [])
+
+    return (
+        <div className="my-4 border border-[#391253] p-4 rounded-md">
+            <h1 className="my-4 text-2xl">Other {props.rank} in {props.lineage}:</h1>
+            { related.length > 0 && related.map((taxa: any) => (
+                <span className="ml-4 pill text-wrap">{taxa.scientific_name}</span>
+            )) }
+        </div>
+    )
 }
